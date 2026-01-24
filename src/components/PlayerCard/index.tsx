92a81c75448a52c6a827aeb40d497094c311e2e0
Khamza Khakim <hamzir.04@gmail.com>
2026-01-24 19:09:57 +0500

    Fix UI field

--- src/components/PlayerCard/index.tsx
+++ src/components/PlayerCard/index.tsx
@@ -1,21 +1,15 @@
 import { mapCoordinates } from "@/src/utils/mapCoordinates";
 
 interface PlayerCardProps {
-  x: number;
-  y: number;
-  size: number;
+  //   x: number;
+  //   y: number;
+  //   size: number;
 }
 
-export default function PlayerCard(props: PlayerCardProps) {
-  const { x, y } = mapCoordinates(props.x, props.y, props.size, 45, 1000);
+export default function PlayerCard() {
+  //   const { x, y } = mapCoordinates(props.x, props.y, props.size, 45, 1000);
 
   return (
-    <div
-      className="h-20 aspect-3/4 bg-amber-300 absolute -translate-x-1/2 -translate-y-1/2 z-10"
-      style={{
-        top: `${y}px`,
-        left: `${x}px`,
-      }}
-    ></div>
+    <div className="h-24 aspect-3/4 bg-amber-300 z-30 border -translate-y-1/2"></div>
   );
 }
--- src/components/Transfers/index.tsx
+++ src/components/Transfers/index.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import PlayerCard from "../PlayerCard";
+import { mapCoordinates } from "@/src/utils/mapCoordinates";
 
 //TODO:
 // - They need to be draggable to make substitutions (key functionality)
@@ -9,18 +10,69 @@ import PlayerCard from "../PlayerCard";
 interface TransfersProps {
   size?: number;
   perspective?: number;
+  rotation?: number;
 }
 
 export default function Transfers({
-  size = 500,
+  size = 600,
   perspective = 800,
+  rotation = 30,
 }: TransfersProps) {
+  const { x: leftX, y: topY } = mapCoordinates(
+    0,
+    600,
+    size,
+    rotation,
+    perspective,
+  );
+  const { x: rightX, y: bottomY } = mapCoordinates(
+    600,
+    600,
+    size,
+    rotation,
+    perspective,
+  );
+
+  console.log(topY, bottomY);
+  console.log(leftX, rightX);
+
   return (
     <div
-      className="mt-24 relative"
-      style={{ perspective: perspective, position: "relative" }}
+      className="m-16 relative overflow-hidden"
+      style={{ perspective: perspective }}
     >
-      <PlayerCard x={0} y={0} size={size} />
+      <div
+        className="z-10 w-20 backdrop-blur-md absolute flex flex-col justify-between p-4"
+        style={{
+          top: topY,
+          left: leftX,
+          height: size - topY,
+          width: size - 2 * leftX,
+        }}
+      >
+        <div id="gk" className="flex justify-center">
+          <PlayerCard />
+        </div>
+        <div id="def" className="flex justify-around px-[16px]">
+          <PlayerCard />
+          <PlayerCard />
+          <PlayerCard />
+          {/* <PlayerCard /> */}
+          {/* <PlayerCard /> */}
+        </div>
+        <div id="mid" className="flex justify-around">
+          <PlayerCard />
+          <PlayerCard />
+          <PlayerCard />
+          <PlayerCard />
+          {/* <PlayerCard /> */}
+        </div>
+        <div id="fwd" className="flex justify-around px-[64px]">
+          <PlayerCard />
+          <PlayerCard />
+          <PlayerCard />
+        </div>
+      </div>
       <Image
         src={"field.svg"}
         alt="Football field"
@@ -28,7 +80,7 @@ export default function Transfers({
         width={size}
         style={{
           position: "relative",
-          transform: "rotateX(45deg)",
+          transform: `rotateX(${rotation}deg)`,
           transformStyle: "preserve-3d",
           zIndex: 0,
         }}
